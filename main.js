b7f95f159a6446108f1b705a6f054135b1342702
Linncharm <linncharm9@gmail.com>
2024-11-09 20:36:29 +0800

    have a try of dark theme

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
-import { app, BrowserWindow, globalShortcut } from 'electron'
-import path from 'node:path'
-//import { VUEJS3_DEVTOOLS } from 'electron-devtools-installer/src'
-//import install from 'electron-devtools-installer/src'
-import { session } from 'electron'
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron/main')
+const path = require('node:path')
 
-const createWindow = () => {
+function createWindow () {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-        // webPreferences: {
-        //     preload: path.join(__dirname, 'preload.js')
-        // }
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
 }
 
-const vueDevToolsPath = 'C:/Users/linnc/AppData/Local/Google/Chrome/User Data/Default/Extensions/nhdogjmejiglipccpnnnanhbledajbpd/7.6.3_1'
+ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+        nativeTheme.themeSource = 'light'
+    } else {
+        nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+})
 
-// Register a global shortcut to open dev tools
-function registryShortcut() {
-    globalShortcut.register('shift+CommandOrControl+m+k', () => {
-        BrowserWindow.getFocusedWindow()?.webContents?.openDevTools();
-    });
-}
+ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+})
 
-app.whenReady().then(async () => {
-    /*try {
-        const name = await install(VUEJS3_DEVTOOLS, true)
-        console.log(`Added Extension: ${name}`)
-    } catch (err) {
-        console.error('Failed to install extension:', err)
-    }*/
-    await session.defaultSession.loadExtension(vueDevToolsPath)
-        .then(() => {
-            console.log('Vue DevTools loaded')
-        })
-    createWindow();
-    registryShortcut();
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
